cf3826bf7a94a162871fb39610b1784c674d0ea3
Joe <jonathan.fauchoux@gmail.com>
2020-08-27 00:19:33 +0200

    sw fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,7 +70,7 @@ workbox.setConfig({
 
 
   
-  /* These JavaScript module imports need to be bundled:
+// These JavaScript module imports need to be bundled:
 import {precacheAndRoute} from 'workbox-precaching';
 import {registerRoute} from 'workbox-routing';
 import {CacheFirst} from 'workbox-strategies';
@@ -79,16 +79,16 @@ import {ExpirationPlugin} from 'workbox-expiration';
 // Use the imported Workbox libraries to implement caching,
 // routing, and other logic:
 precacheAndRoute(self.__WB_MANIFEST);
-/* registerRoute(
+/*  registerRoute(
   ({request}) => request.destination === 'image',
   new CacheFirst({cacheName: 'images'}),
 );
 registerRoute(
   ({request}) => request.destination === 'image',
   ({cacheName: 'images'}),
-); */
+); */ 
 
-/* Etc.
+// Etc.
 registerRoute(
   /\.(?:png|gif|jpg|jpeg|svg)$/,
   new CacheFirst({
@@ -113,4 +113,4 @@ registerRoute(
       }),
     ],
   })
-); */
+); 
