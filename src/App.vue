a0d2f0a2a3c6d099ad80855771bfbd3e62256845
Joe <jonathan.fauchoux@gmail.com>
2020-08-26 10:31:54 +0200

    btn install pwa

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,23 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+         <InstallBtn />
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import InstallBtn from '@/components/InstallBtn.vue'
 
+export default {
+  name: 'app',
+  components: {
+    InstallBtn
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/InstallBtn.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="installBtn">
+   <a href="#" @click.prevent="install()">Install App</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InstallBtn',
+  data(){
+    return{
+      show :true,
+      defferedPromt:null,
+      installBtn : document.querySelector('.install')
+    }
+  },
+  methods:{
+    install(){
+
+      //installBtn.style.display ="none"
+      this.defferedPromt.prompt()
+
+      this.defferedPromt.userChoice.then(choiceResult => {
+      if(choiceResult.outcome === 'accepted'){
+      console.log(('installation réussie'))
+      }else{
+      console.log(('installation refusée'))
+      }
+      this.defferedPromt
+      })
+    }
+  },
+  created(){
+    
+   
+    window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    this.defferedPromt = e
+    //installBtn.style.display = "block"
+})
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
